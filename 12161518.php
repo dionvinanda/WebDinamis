8be6bdc738615ee2f05769700bc9d366a19e571b
dionvinanda <49020660+dionvinanda@users.noreply.github.com>
2019-03-28 15:27:02 +0700

    Add files via upload
    Tugas Web Dinamis membuat array multi dimensi 4

---
+++ 12161518.php
@@ -0,0 +1,16 @@
+<?php
+
+	$buah_buahan = [
+		['apel' => ['warna' => 'merah', 'bentuk' => 'bulat', 'rasa' => 'manis']], 
+		['pisang' => ['warna' => 'kuning', 'bentuk' => 'lonjong', 'rasa' => 'manis']], 
+		['semangka' => ['warna' => 'hijau', 'bentuk' => 'bulat', 'rasa' => 'manis']], 
+		['jeruk' => ['warna' => 'kuning', 'bentuk' => 'bulat', 'rasa' => 'asam']], 
+		['pir' => ['warna' => 'hijau', 'bentuk' => 'oval', 'rasa' => 'manis']], 
+		
+	];
+
+	for ($i=0;$i<1;$i++)
+	{
+		echo '<pre>'; print_r($buah_buahan);
+	};
+?>
